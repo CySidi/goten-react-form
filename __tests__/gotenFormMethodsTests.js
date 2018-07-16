1edef1f6575514c2c024f9d3ec459eabedb26bc2
smeilan <smeilan@cys.com.ar>
2018-07-16 12:57:16 -0300

    Quitar sleep tests
    - Se quitaron los sleeps, ya que hacian que los tests pierdan eficacia, y a veces fallaban cuando no tendrian que haber fallado.
    - Se espera a que se reciban todos los mensajes por parte de los GotenTextFields para luego continuar con las validaciones.

--- __tests__/gotenFormMethodsTests.js
+++ __tests__/gotenFormMethodsTests.js
@@ -1,18 +1,35 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 
+import { PubSub } from 'pubsub-js'
+
 import { GotenTextField } from 'goten-react-text-field'
 import { GotenForm } from '../src'
 
-const time = 100
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
+const messageResponse = '_RESPONSE'
+let actualMessage = 0
+let pubsub = undefined
+
+function executeWhenAllMessagesAreRecived(message, numberOfmessages, code) {
+    pubsub = PubSub.subscribe(message + messageResponse, _ => {
+        ++actualMessage
+        if (actualMessage == numberOfmessages) {
+            code()
+        }
+    })
 }
 
 describe('GotenForm validate tests', () => {
+ 
+    afterEach(() => {
+        if (pubsub)
+            PubSub.unsubscribe(pubsub)
+        actualMessage = 0
+        pubsub = undefined
+    })
 
-    it('check onSucces', async () => {
+    it('check onSucces', done => {
         const type = 'text'
         const label = 'Text'
         const value = 'it is a value'
@@ -27,12 +44,14 @@ describe('GotenForm validate tests', () => {
                 />
             </GotenForm>
         ).getInstance()
+        executeWhenAllMessagesAreRecived(component.pubsubMessage, component.gotenTextFieldCant, _ => {
+            expect(component.props.onSucces).toHaveBeenCalledTimes(1)
+            done()
+        })
         component.validate()
-        await sleep(time)
-        expect(component.props.onSucces).toHaveBeenCalledTimes(1)
     })
 
-    it('check onSucces with onError', async () => {
+    it('check onSucces with onError', done => {
         const type = 'text'
         const label = 'Text'
         const value = 'it is a value'
@@ -48,12 +67,14 @@ describe('GotenForm validate tests', () => {
                 />
             </GotenForm>
         ).getInstance()
+        executeWhenAllMessagesAreRecived(component.pubsubMessage, component.gotenTextFieldCant, _ => {
+            expect(component.props.onSucces).toHaveBeenCalledTimes(1)
+            done()
+        })
         component.validate()
-        await sleep(time)
-        expect(component.props.onSucces).toHaveBeenCalledTimes(1)
     })
 
-    it('check onError', async () => {
+    it('check onError', done => {
         const type = 'email'
         const label = 'Text'
         const value = 'it is a value'
@@ -69,13 +90,15 @@ describe('GotenForm validate tests', () => {
                 />
             </GotenForm>
         ).getInstance()
+        executeWhenAllMessagesAreRecived(component.pubsubMessage, component.gotenTextFieldCant, _ => {
+            expect(component.props.onSucces).toHaveBeenCalledTimes(0)
+            expect(component.props.onError).toHaveBeenCalledTimes(1)
+            done()
+        })
         component.validate()
-        await sleep(time)
-        expect(component.props.onSucces).toHaveBeenCalledTimes(0)
-        expect(component.props.onError).toHaveBeenCalledTimes(1)
     })
 
-    it('check error without onError', async () => {
+    it('check error without onError', done => {
         const type = 'email'
         const label = 'Text'
         const value = 'it is a value'
@@ -90,12 +113,14 @@ describe('GotenForm validate tests', () => {
                 />
             </GotenForm>
         ).getInstance()
+        executeWhenAllMessagesAreRecived(component.pubsubMessage, component.gotenTextFieldCant, _ => {
+            expect(component.props.onSucces).toHaveBeenCalledTimes(0)
+            done()
+        })
         component.validate()
-        await sleep(time)
-        expect(component.props.onSucces).toHaveBeenCalledTimes(0)
     })
 
-    it('check onSucces with buttonComponent', async () => {
+    it('check onSucces with buttonComponent', done => {
         const type = 'text'
         const label = 'Text'
         const value = 'it is a value'
@@ -115,12 +140,15 @@ describe('GotenForm validate tests', () => {
                 />
             </GotenForm>
         )
+        const componentInstance = component.getInstance()
+        executeWhenAllMessagesAreRecived(componentInstance.pubsubMessage, componentInstance.gotenTextFieldCant, _ => {
+            expect(componentInstance.props.onSucces).toHaveBeenCalledTimes(1)
+            done()
+        })
         component.root.findByType('button').props.onClick()
-        await sleep(time)
-        expect(component.getInstance().props.onSucces).toHaveBeenCalledTimes(1)
     })
 
-    it('check onSucces with onError with buttonComponent', async () => {
+    it('check onSucces with onError with buttonComponent', done => {
         const type = 'text'
         const label = 'Text'
         const value = 'it is a value'
@@ -141,12 +169,15 @@ describe('GotenForm validate tests', () => {
                 />
             </GotenForm>
         )
+        const componentInstance = component.getInstance()
+        executeWhenAllMessagesAreRecived(componentInstance.pubsubMessage, componentInstance.gotenTextFieldCant, _ => {
+            expect(componentInstance.props.onSucces).toHaveBeenCalledTimes(1)
+            done()
+        })        
         component.root.findByType('button').props.onClick()
-        await sleep(time)
-        expect(component.getInstance().props.onSucces).toHaveBeenCalledTimes(1)
     })
 
-    it('check onError with buttonComponent', async () => {
+    it('check onError with buttonComponent', done => {
         const type = 'email'
         const label = 'Text'
         const value = 'it is a value'
@@ -167,13 +198,16 @@ describe('GotenForm validate tests', () => {
                 />
             </GotenForm>
         )
+        const componentInstance = component.getInstance()
+        executeWhenAllMessagesAreRecived(componentInstance.pubsubMessage, componentInstance.gotenTextFieldCant, _ => {
+            expect(componentInstance.props.onSucces).toHaveBeenCalledTimes(0)
+            expect(componentInstance.props.onError).toHaveBeenCalledTimes(1)
+            done()
+        })
         component.root.findByType('button').props.onClick()
-        await sleep(time)
-        expect(component.getInstance().props.onSucces).toHaveBeenCalledTimes(0)
-        expect(component.getInstance().props.onError).toHaveBeenCalledTimes(1)
     })
 
-    it('check error without onError with buttonComponent', async () => {
+    it('check error without onError with buttonComponent', done => {
         const type = 'email'
         const label = 'Text'
         const value = 'it is a value'
@@ -193,8 +227,11 @@ describe('GotenForm validate tests', () => {
                 />
             </GotenForm>
         )
+        const componentInstance = component.getInstance()
+        executeWhenAllMessagesAreRecived(componentInstance.pubsubMessage, componentInstance.gotenTextFieldCant, _ => {
+            expect(componentInstance.props.onSucces).toHaveBeenCalledTimes(0)
+            done()
+        })        
         component.root.findByType('button').props.onClick()
-        await sleep(time)
-        expect(component.getInstance().props.onSucces).toHaveBeenCalledTimes(0)
     })
 })
